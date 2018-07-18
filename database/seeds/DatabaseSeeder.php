6623e7274987d2dc7fe107b428305055ca6d4d32
Jayden Chua <jayden.chua@modix.cn>
2018-07-18 10:32:03 +0800

    make employee migration and seed files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Employee;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Employee::query()->delete();
+        $this->call(EmployeesTableSeeder::class);
     }
 }
