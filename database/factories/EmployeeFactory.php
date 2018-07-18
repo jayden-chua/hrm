38aa835ff8763719e1924eefd8fec2214c85f5f4
Jayden Chua <jayden.chua@modix.cn>
2018-07-18 10:45:35 +0800

    used firstname and lastname instead of generic name

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -2,22 +2,11 @@
 
 use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(App\Employee::class, function (Faker $faker) {
     return [
-        'firstname' => $faker->name,
-        'lastname' => $faker->name,
-        'email' => $faker->unique()->companyEmail,
+        'firstname' => $faker->firstName,
+        'lastname' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
         'mobile' => $faker->unique()->phoneNumber
     ];
 });
